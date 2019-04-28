f24fa9816c4bd2c100e91c83331d0d12a4c59177
jklug34 <48166327+jklug34@users.noreply.github.com>
2019-04-28 00:33:39 -0700

    Add files via upload

--- Multiple_year_stock_data_JK_ModeratewChallenge.vb
+++ Multiple_year_stock_data_JK_ModeratewChallenge.vb
@@ -21,11 +21,11 @@ Sub StockMarketModerate():
 
 'Define variables
 Dim Ticker As String
-Dim Total_Volume As Long
+Dim Total_Volume As Double
 Dim i As Long
 Dim Volume As Long
 Dim LastRow As Long
-Dim Summary_Table_Row As Long
+Dim Summary_Table_Row As Integer
 Dim WorksheetName As String
 Dim ws As Worksheet
 Dim OpeningValue As Double
@@ -83,114 +83,111 @@ Summary_Table_Row = 2
         For i = 2 To LastRow
             
                 
-        'Check if we are still within the same ticker symbol, if not...
-        If ws.Cells(i + 1, 1).Value <> ws.Cells(i, 1).Value Then '<> is not equal to, cell (3,2) is not equal to cell (2,2) and so on...
-                        
-        'Set the ticker symbol
-        Ticker = ws.Cells(i, 1).Value
-        
-        'Grab the Opening Value for each ticker
-        ClosingValue = ws.Cells(i, 6).Value
-        'Debug.Print ClosingValue
-                                
-        'Add row ticker volume to the final stock ticker volume
-        Total_Volume = Total_Volume + ws.Cells(i, 7).Value
-                        
-        'Print the Ticker symbol in the summary table
-        ws.Range("I" & Summary_Table_Row).Value = Ticker
-                
-        'Print the Yearly_Change in the summary table
-        ws.Range("J" & Summary_Table_Row).Value = Yearly_Change
-                
-        'PrBCint the Percent_Change in the summary table
-        ws.Range("K" & Summary_Table_Row).Value = Percent_Change
-                
-        'Print the total volume for that ticker symbol in the summary table
-        ws.Range("L" & Summary_Table_Row).Value = Total_Volume
-                
-        'Add one to the summary table row for each new ticker
-        Summary_Table_Row = Summary_Table_Row + 1
+            'Check if we are still within the same ticker symbol, if not...
+            If ws.Cells(i + 1, 1).Value <> ws.Cells(i, 1).Value Then '<> is not equal to, cell (3,2) is not equal to cell (2,2) and so on...
                             
-        'Reset the ticker symbol total volume for next ticker symbol
-        Total_Volume = 0
-        OpeningValue = 0
-        
-                    
-        'If the ticker symbol in the next row is the same then..
-        Else
-                    
-        'Add the ticker volume
-        Total_Volume = Total_Volume + ws.Cells(i, 7).Value
-        
-        'Grab the Opening Value for each ticker
-        OpeningValueAll = Array(ws.Cells(i, 3).Value)
-        OpeningValue = OpeningValueAll(1) 'find the first element of the array
-        'Debug.Print OpeningValue
-        
-        'Define the Yearly Change in Value as the Closing Value - the Opening Value
-         Yearly_Change = ClosingValue - OpeningValue
-        
-        'Define the Percent Change as the Yearly_Change - the Opening Value
-         Percent_Change = (Yearly_Change / OpeningValue) * 100
-                        
-        'Reset the ticker symbol total volume for next ticker symbol
-        Total_Volume = 0
-        OpeningValue = 0
-        ClosingValue = 0
-        Yearly_Change = 0
-        Percent_Change = 0
-        
+                'Set the ticker symbol
+                Ticker = ws.Cells(i, 1).Value
                 
-        End If
+                'Grab the Opening Value for each ticker
+                ClosingValue = ws.Cells(i, 6).Value
+                'Debug.Print ClosingValue
+                                        
+                'Add row ticker volume to the final stock ticker volume
+                Total_Volume = Total_Volume + ws.Cells(i, 7).Value
+                                
+                'Print the Ticker symbol in the summary table
+                ws.Range("I" & Summary_Table_Row).Value = Ticker
+                        
+                'Print the Yearly_Change in the summary table
+                ws.Range("J" & Summary_Table_Row).Value = Yearly_Change
+                        
+                'PrBCint the Percent_Change in the summary table
+                ws.Range("K" & Summary_Table_Row).Value = Percent_Change
+                        
+                'Print the total volume for that ticker symbol in the summary table
+                ws.Range("L" & Summary_Table_Row).Value = Total_Volume
+                        
+                'Add one to the summary table row for each new ticker
+                Summary_Table_Row = Summary_Table_Row + 1
+                                    
+                'Reset the ticker symbol total volume for next ticker symbol
+                Total_Volume = 0
+                OpeningValue = 0
+                ClosingValue = 0
+                Yearly_Change = 0
+                Percent_Change = 0
+            
+                        
+            'If the ticker symbol in the next row is the same then..
+            Else
+                        
+                'Add the ticker volume
+                Total_Volume = Total_Volume + ws.Cells(i, 7).Value
+                
+                
+            End If
+            
+                'Grab the Opening Value for each ticker
+                OpeningValueAll = Array(ws.Cells(i, 3).Value)
+                OpeningValue = OpeningValueAll(1) 'find the first element of the array
+                'Debug.Print OpeningValue
+                
+                'Define the Yearly Change in Value as the Closing Value - the Opening Value
+                Yearly_Change = ClosingValue - OpeningValue
+                
+                'Define the Percent Change as the Yearly_Change - the Opening Value
+                Percent_Change = (Yearly_Change / OpeningValue) * 100
                   
         Next i
                         
-        'Reset the Summary_Table_Row back to 2 for the next worksheet
-        Summary_Table_Row = 2
+            'Reset the Summary_Table_Row back to 2 for the next worksheet
+            Summary_Table_Row = 2
         
-'        Set as Percent
-        '-------------------------------------------------------
-
-         ws.Range("K2:K" & LastRow).Style = "Percent"
-'
-'
-'        ActiveCell.FormulaR1C1 = "=RC[-1]/RC[-8]"
-'        Selection.NumberFormat = "0.00%"
-'
-'        Set the conditional formatting rules
-         '-----------------------------------------------------
-'
-         If Percent_Change > 0 Then
-'
-         ws.Range("J" & Summary_Table_Row).Interior.ColorIndex = 4
-'
-         Else
-         ws.Range("J" & Summary_Table_Row).Interior.ColorIndex = 3
-
-         End If
-         
-         
-         'OR
-'
-'            Columns("J:J").Select
-'        Selection.FormatConditions.Add Type:=xlCellValue, Operator:=xlGreater, _
-'            Formula1:="=0"
-'        Selection.FormatConditions(Selection.FormatConditions.Count).SetFirstPriority
-'        With Selection.FormatConditions(1).Interior
-'            .PatternColorIndex = xlAutomatic
-'            .Color = 5296274
-'            .TintAndShade = 0
-'        End With
-'        Selection.FormatConditions(1).StopIfTrue = False
-'        Selection.FormatConditions.Add Type:=xlCellValue, Operator:=xlLess, _
-'            Formula1:="=0"
-'        Selection.FormatConditions(Selection.FormatConditions.Count).SetFirstPriority
-'        With Selection.FormatConditions(1).Interior
-'            .PatternColorIndex = xlAutomatic
-'            .Color = 255
-'            .TintAndShade = 0
-'        End With
-'        Selection.FormatConditions(1).StopIfTrue = False
+        '        Set as Percent
+                '-------------------------------------------------------
+        
+                 ws.Range("K2:K" & LastRow).Style = "Percent"
+        '
+                 'OR
+        '
+        '        ActiveCell.FormulaR1C1 = "=RC[-1]/RC[-8]"
+        '        Selection.NumberFormat = "0.00%"
+        '
+        '        Set the conditional formatting rules
+                 '-----------------------------------------------------
+        '
+                 If Percent_Change > 0 Then
+        '
+                    ws.Range("J" & Summary_Table_Row).Interior.ColorIndex = 4
+        '
+                 Else
+                    ws.Range("J" & Summary_Table_Row).Interior.ColorIndex = 3
+        
+                 End If
+                 
+                 
+                 'OR
+        '
+        '            Columns("J:J").Select
+        '        Selection.FormatConditions.Add Type:=xlCellValue, Operator:=xlGreater, _
+        '            Formula1:="=0"
+        '        Selection.FormatConditions(Selection.FormatConditions.Count).SetFirstPriority
+        '        With Selection.FormatConditions(1).Interior
+        '            .PatternColorIndex = xlAutomatic
+        '            .Color = 5296274
+        '            .TintAndShade = 0
+        '        End With
+        '        Selection.FormatConditions(1).StopIfTrue = False
+        '        Selection.FormatConditions.Add Type:=xlCellValue, Operator:=xlLess, _
+        '            Formula1:="=0"
+        '        Selection.FormatConditions(Selection.FormatConditions.Count).SetFirstPriority
+        '        With Selection.FormatConditions(1).Interior
+        '            .PatternColorIndex = xlAutomatic
+        '            .Color = 255
+        '            .TintAndShade = 0
+        '        End With
+        '        Selection.FormatConditions(1).StopIfTrue = False
                         
     Next
 
